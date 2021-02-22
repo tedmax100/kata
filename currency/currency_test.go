ad9cc1275cd3e23a4672a454dfd5dc7f4b8352f1
Nathan <tedmax100@gmail.com>
2021-02-22 23:43:21 +0800

    deletet duplcate test cases

--- currency/currency_test.go
+++ currency/currency_test.go
@@ -6,23 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDollarMultiplication(t *testing.T) {
+/* func TestDollarMultiplication(t *testing.T) {
 	five := Dollar(5)
 	assert.True(t, Dollar(10).Equals(five.Times(2)))
 	assert.True(t, Dollar(15).Equals(five.Times(3)))
 	assert.False(t, Frac(5).Equals(Frac(6)))
 	assert.False(t, Frac(5).Equals(Dollar(5)))
 }
-
-func TestFranceMultiplication(t *testing.T) {
+*/
+/* func TestFranceMultiplication(t *testing.T) {
 	five := Frac(5)
 	assert.True(t, Frac(10).Equals(five.Times(2)))
 	assert.True(t, Frac(15).Equals(five.Times(3)))
-}
+} */
 
 func TestEquality(t *testing.T) {
 	assert.True(t, Dollar(5).Equals(Dollar(5)))
 	assert.False(t, Dollar(5).Equals(Dollar(6)))
+	assert.False(t, Frac(5).Equals(Dollar(5)))
 }
 
 func TestCurrency(t *testing.T) {
