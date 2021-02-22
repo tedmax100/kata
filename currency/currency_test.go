48e9c5f790922bed1c083ae31d2dcc0553fea5d3
Nathan <tedmax100@gmail.com>
2021-02-22 15:14:03 +0800

    correct assert bool usage

--- currency/currency_test.go
+++ currency/currency_test.go
@@ -8,17 +8,19 @@ import (
 
 func TestDollarMultiplication(t *testing.T) {
 	five := NewDollar(5)
-	assert.True(t, true, NewDollar(10).Equals(five.Times(2)))
-	assert.True(t, true, NewDollar(15).Equals(five.Times(3)))
+	assert.True(t, NewDollar(10).Equals(five.Times(2)))
+	assert.True(t, NewDollar(15).Equals(five.Times(3)))
+	assert.False(t, NewFranc(5).Equals(NewFranc(6)))
+	//assert.False(t, NewFranc(5).Equals(NewDollar(5)))
 }
 
 func TestFranceMultiplication(t *testing.T) {
 	five := NewFranc(5)
-	assert.True(t, true, NewFranc(10).Equals(five.Times(2)))
-	assert.True(t, true, NewFranc(15).Equals(five.Times(3)))
+	assert.True(t, NewFranc(10).Equals(five.Times(2)))
+	assert.True(t, NewFranc(15).Equals(five.Times(3)))
 }
 
 func TestEquality(t *testing.T) {
-	assert.True(t, true, NewDollar(5).Equals(NewDollar(5)))
-	assert.False(t, false, NewDollar(5).Equals(NewDollar(6)))
+	assert.True(t, NewDollar(5).Equals(NewDollar(5)))
+	assert.False(t, NewDollar(5).Equals(NewDollar(6)))
 }
