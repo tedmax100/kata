57400a194489bdc151942a5ca2ec23fbc883c2bc
Nathan <tedmax100@gmail.com>
2021-02-28 22:43:06 +0800

    add sumOfBallsInFrame, getSpareBonus, getStrikeBonus

--- bowling/game.go
+++ bowling/game.go
@@ -24,18 +24,29 @@ func (g *Game) Score() int {
 	var frameIdx int = 0 // i is a bad name for this variable
 	for frame := 0; frame < 10; frame++ {
 		if g.rolls[frameIdx] == 10 { // strike
-			score += 10 + g.rolls[frameIdx+1] + g.rolls[frameIdx+2]
+			score += 10 + g.getStrikeBonus(frameIdx)
 			frameIdx++
 		} else if g.isSpare(frameIdx) {
-			score += 10 + g.rolls[frameIdx+2]
+			score += 10 + g.getSpareBonus(frameIdx)
 			frameIdx += 2
 		} else {
-
-			score += g.rolls[frameIdx] + g.rolls[frameIdx+1]
+			score += g.sumOfBallsInFrame(frameIdx)
 			frameIdx += 2
 		}
 	}
 	return score
+}
+
+func (g *Game) sumOfBallsInFrame(frameIdx int) int {
+	return g.rolls[frameIdx] + g.rolls[frameIdx+1]
+}
+
+func (g *Game) getSpareBonus(frameIdx int) int {
+	return g.rolls[frameIdx+2]
+}
+
+func (g *Game) getStrikeBonus(frameIdx int) int {
+	return g.rolls[frameIdx+1] + g.rolls[frameIdx+2]
 }
 
 func (g *Game) isSpare(frameIdx int) bool {
