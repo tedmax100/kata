3f7cd6319cc148d763ff288c533fb7035f517664
Nathan <tedmax100@gmail.com>
2021-02-28 22:14:47 +0800

    add calcuate spare and normal score in Score()

--- bowling/game.go
+++ bowling/game.go
@@ -1,20 +1,38 @@
 package bowling
 
 type Game struct {
-	score int
+	score       int
+	rolls       []int // to store maximum 22 pins of rolls
+	currentRoll int
 }
 
 func NewGame() *Game {
 	return &Game{
-		score: 0,
+		score:       0,
+		rolls:       make([]int, 21),
+		currentRoll: 0,
 	}
 }
 
 func (g *Game) Roll(pins int) {
-	g.score += pins // roll職責錯誤, 它不應該去累加score
-	return
+	g.rolls[g.currentRoll] = pins
+	g.currentRoll++
 }
 
 func (g *Game) Score() int {
-	return g.score //score() 反而沒負責計算分數
+	var score int = 0
+	var i int = 0
+	for frame := 0; frame < 10; frame++ {
+		if g.rolls[i]+g.rolls[i+1] == 10 { //spare
+			score += 10 + g.rolls[i+2]
+			i += 2
+		} else {
+
+			score += g.rolls[i] + g.rolls[i+1]
+			i += 2
+		}
+
+	}
+
+	return score
 }
