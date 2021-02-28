b2220e8df69771cef44e6740858fe3e0b340e875
Nathan <tedmax100@gmail.com>
2021-02-28 22:35:45 +0800

    finish strike logic for score()

--- bowling/game.go
+++ bowling/game.go
@@ -23,7 +23,10 @@ func (g *Game) Score() int {
 	var score int = 0
 	var frameIdx int = 0 // i is a bad name for this variable
 	for frame := 0; frame < 10; frame++ {
-		if g.isSpare(frameIdx) {
+		if g.rolls[frameIdx] == 10 { // strike
+			score += 10 + g.rolls[frameIdx+1] + g.rolls[frameIdx+2]
+			frameIdx++
+		} else if g.isSpare(frameIdx) {
 			score += 10 + g.rolls[frameIdx+2]
 			frameIdx += 2
 		} else {
