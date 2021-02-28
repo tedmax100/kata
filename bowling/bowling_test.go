f3bd4ec1a38be6dbbe4b8088d1d284e29fb275af
Nathan <tedmax100@gmail.com>
2021-02-28 16:33:36 +0800

    Add TestAllOnes and reduce logic to rollMany

--- bowling/bowling_test.go
+++ bowling/bowling_test.go
@@ -7,9 +7,23 @@ import (
 )
 
 func TestGutterGame(t *testing.T) {
-	var game Game = NewGame()
-	for i := 0; i < 20; i++ {
-		game.Roll(0)
-	}
+	var game *Game = NewGame()
+	var n int = 20
+	var pins int = 0
+	rollMany(game, n, pins)
 	assert.Equal(t, 0, game.Score())
+}
+
+func TestAllOnes(t *testing.T) {
+	var game *Game = NewGame()
+	var n int = 20
+	var pins int = 1
+	rollMany(game, n, pins)
+	assert.Equal(t, 20, game.Score())
+}
+
+func rollMany(game *Game, n, pins int) {
+	for i := 0; i < n; i++ {
+		game.Roll(pins)
+	}
 }
--- bowling/game.go
+++ bowling/game.go
@@ -1,16 +1,20 @@
 package bowling
 
 type Game struct {
+	score int
 }
 
-func NewGame() Game {
-	return Game{}
+func NewGame() *Game {
+	return &Game{
+		score: 0,
+	}
 }
 
-func (g Game) Roll(pins int) {
+func (g *Game) Roll(pins int) {
+	g.score += pins
 	return
 }
 
-func (g Game) Score() int {
-	return 0
+func (g *Game) Score() int {
+	return g.score
 }
