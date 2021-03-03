33df7d255e4c3d48151530c170b176fdbc5ca714
Nathan <tedmax100@gmail.com>
2021-03-03 13:24:01 +0800

    add TestOnes

--- yahtzee/yahtzee_test.go
+++ yahtzee/yahtzee_test.go
@@ -11,3 +11,8 @@ func TestChance(t *testing.T) {
 	assert.Equal(t, 15, rule.Chance([]int{2, 3, 4, 5, 1}))
 	assert.Equal(t, 16, rule.Chance([]int{3, 3, 4, 5, 1}))
 }
+
+func TestOnes(t *testing.T) {
+	var rule *Rule = &Rule{}
+	assert.Equal(t, 1, rule.Once([]int{1, 2, 3, 4, 5}))
+}
